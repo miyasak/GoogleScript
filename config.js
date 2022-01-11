11737f5b1a0f99ceff1238977f6024607ea2f49d
miyasaka <miyasaka@t-garden.jp>
2022-01-11 12:25:10 +0900

    addnewfile

---
+++ config.js
@@ -0,0 +1,12 @@
+/**
+ * マスタシートへ渡す条件となるモード指定
+ * @type {string}
+ */
+const TARGET_MODE = "REQUIRED";
+
+// テーブル定義の項目名判定に利用
+const FIELD_NAME = "フィールド名";
+const FIELD_TYPE = "種類";
+const FIELD_MODE = "モード名";
+const FIELD_DESCRIPTION = "説明";
+
---
+++ setting.js
@@ -0,0 +1,261 @@
+/**
+ * Whats this?
+ * BigQueryからデータをスプレッドシート(以下、参照先スプレッドシート)のうち、必須項目に対してデータ入力規則と入力規則をセットします
+ *
+ * !!!caution!!!
+ * 各シートのA1セルには、参照先スプレッドシートのリンクを埋め込んでください!!
+ * 各シートのヘッダはA列：フィールド名、B列：種類、C列：モード、D列：説明の順にセットしてください!!
+ * 参照先スプレッドシートにセットする入力書式もしくはデータ入力規則のカスタム関数を変更する場合は、settingオブジェクトの値を書き換えてください!!
+ * モード指定を変更する場合は、TARGET_MODEの変数名を書き換えてください。(複数のモードには未対応です)
+ * 参照先スプレッドシートの一番左のシートにBigQueryから連携するデータをセットしてください。
+ */
+
+// グローバル変数
+
+/**
+ * masterテーブルの情報を管理するオブジェクト
+ * @type {{}}
+ */
+let tableObj = {};
+
+/**
+ * テーブルデータのうち、必須項目(REQUIRED)のフィールドを格納する配列
+ * マスタシートへ入力規則と書式設定を反映する時に使います
+ * @type {*[]}
+ * Settings：種類(type)[0], 説明(description)[1], モード(mode)[2]
+ */
+let requiredArray = [];
+
+/**
+ * テーブル定義の項目名チェックフラグ
+ * @type {boolean}
+ */
+let tableCategoryFlag = false;
+
+/**
+ * ハイパーリンクのセル指定に利用する変数
+ * @type {string}
+ */
+let cellNotation = "";
+
+function Main() {
+
+    // テーブル定義の各シートデータを連想配列へ格納する
+    _pushTableData();
+
+    // テーブル情報を取得できていれば、後続処理を開始します
+    if (tableCategoryFlag) {
+
+        for (let key in tableObj) {
+
+            // 全テーブルデータのうち、REQUIREDモードのフィールドだけを抽出し直します
+            _setRequiredData(key);
+
+            // 参照先スプレッドシートの一番左のシートを参照します
+            let targetSheet = _getReferenceSheet();
+
+            // 参照先スプレッドシートの最終行、最終列をそれぞれ取得します
+            let targetRange = _getSheetRange(targetSheet);
+            // 参照先スプレッドシートの最終行
+            let lastRow = targetRange[0];
+            // 参照先スプレッドシートの最終列
+            let lastColumn = targetRange[1];
+
+            // 参照先スプレッドシートの必須項目列に対して、入力書式と入力規則をそれぞれセットします
+            _setCustomFunction(lastRow, lastColumn, targetSheet);
+            // 参照シートのカスタム関数セット完了したら、配列を空にします
+            requiredArray = [];
+        }
+    }
+}
+
+/**
+ * @private
+ * テーブル定義のシートデータを連想配列として格納します
+ * tableObjはグローバルオブジェクトです
+ */
+function _pushTableData() {
+
+    let sheets = SpreadsheetApp.getActiveSpreadsheet().getSheets();
+    // スプレッドシートのシートを全て取得して、ループ。シート分処理を繰り返します
+    for (let count = 0; count < sheets.length; count++) {
+        tableObj["sheetNum" + num] = {}; // シートNo(キー)
+        tableObj["sheetNum" + num]["sheetName"] = sheets[num].getName(); // シート名
+        let lastRow = sheets[num].getRange(1, 1).getNextDataCell(SpreadsheetApp.Direction.DOWN).getRow(); // 最終行を指定
+
+        // マスタシートA1にセットされているURLを取得します
+        let targetLink = sheets[num].getRange(1, 1).getRichTextValues();
+        tableObj["sheetNum" + num]["sheetLink"] = targetLink[0][0].getLinkUrl();
+
+        // 各データをキー配列として保持できるようにします
+        tableObj["sheetNum" + num]["cellData"] = {
+            field: [], // フィールド名
+            type: [], // 種類
+            mode: [], // モード
+            description: [] // 説明
+        };
+
+        let cellData = sheets[num].getRange(1, 1, lastRow, 4).getValues(); // A列〜D列の最終行を指定
+        // シートの最終行までループさせて、各項目を配列に追加します(スキーマは使わないので除外)
+        for (let sheetRow = 0; sheetRow < cellData.length; sheetRow++) {
+            // もし他の項目を追加する必要があるときは、引数に追加でセットします
+            if (tableCategoryFlag) {
+                // もし他の項目を追加する必要があるときは、引数に追加でセットします
+                tableObj["sheetNum" + num]["cellData"]["field"].push(cellData[sheetRow][0]);
+                tableObj["sheetNum" + num]["cellData"]["type"].push(cellData[sheetRow][1]);
+                tableObj["sheetNum" + num]["cellData"]["mode"].push(cellData[sheetRow][2]);
+                tableObj["sheetNum" + num]["cellData"]["description"].push(cellData[sheetRow][3]);
+            } else {
+                if ((sheetRow == 0) && (cellData[sheetRow][0] === FIELD_NAME) && (cellData[sheetRow][1] === FIELD_TYPE) && (cellData[sheetRow][2] === FIELD_MODE) && (cellData[sheetRow][3] === FIELD_DESCRIPTION)) {
+                    // テーブル定義が正しければ、連想配列へ格納できるようにフラグをtrueにします
+                    tableCategoryFlag = true;
+                } else {
+                    // フィールド名が誤っていた場合は、処理を終了します
+                    console.log("項目名が誤っています!!!");
+                    return;
+                }
+            }
+        }
+    }
+}
+
+/**
+ * テーブルデータのうち、必須項目(REQUIRED)のフィールドを配列へ格納します
+ * @param key
+ * @private
+ */
+function _setRequiredData(key) {
+    // スプレッドシートのシートIDを取得します
+    requiredArray.push(tableObj[key]["sheetLink"].split("/")[5]);
+    // C列のモードが"REQUIRED"となっているフィールドのみ、配列へセットし直します
+    for (let reqCount = 0; reqCount < tableObj[key]["cellData"]["field"].length; reqCount++) {
+        // もし他のモードを追加する必要があるときは、以下に"or"で条件を追加します
+        if ((tableObj[key]["cellData"]["mode"][reqCount] === TARGET_MODE) && (tableObj[key]["cellData"]["type"][reqCount] === "INTEGER" || tableObj[key]["cellData"]["type"][reqCount] === "STRING")) {
+            // もし他の項目を追加する必要があるときは、引数に追加でセットします
+            requiredArray.push([tableObj[key]["cellData"]["type"][reqCount], tableObj[key]["cellData"]["description"][reqCount], tableObj[key]["cellData"]["mode"][reqCount]]);
+        }
+    }
+    requiredArray.push(['DATE','作成日時','REQUIRED']);
+    requiredArray.push(['DATE','更新日時','REQUIRED']);
+    // console.log(requiredArray);
+}
+
+/**
+ * 参照先スプレッドシートの一番左のシートを参照します
+ * @returns {*}
+ * @private
+ */
+function _getReferenceSheet() {
+    let targetSpreadsheet = SpreadsheetApp.openById(requiredArray[0]);
+    let sheet = targetSpreadsheet.getSheets()[0];
+    return sheet;
+}
+
+/**
+ * 参照先スプレッドシートの最終行、最終列をそれぞれ取得します
+ * @returns {*[]}
+ * @private
+ */
+function _getSheetRange(targetSheet) {
+    var row = targetSheet.getRange(1, 1).getNextDataCell(SpreadsheetApp.Direction.DOWN).getRow();
+    var column = targetSheet.getRange(1, 1).getNextDataCell(SpreadsheetApp.Direction.NEXT).getColumn();
+    return [row, column];
+}
+
+/**
+ * 必須項目列に対して、入力書式と入力規則をセットします
+ * @param lastRow
+ * @param lastColumn
+ * @param targetSheet
+ * @private
+ */
+function _setCustomFunction(lastRow, lastColumn, targetSheet) {
+    // 参照先スプレッドシートの最終行まで1行ずつ処理します
+    for (let targetRow = 1; targetRow < lastColumn; targetRow++) {
+
+        // 参照先スプレッドシートA列の項目名を取得します。この値は、requiredArray配列のdescriptionとの比較に使います
+        let headerData = targetSheet.getRange(1, targetRow ,1, 1).getValue();
+        // グローバルの変数に代入し、該当列に対して書式設定とデータ入力をセットできるようにします
+        cellNotation = targetSheet.getRange(1, targetRow ,1, 1).getA1Notation().replace("1","");
+        // console.log(cellNotation);
+
+        /**
+         シートにセットする入力書式と入力関数のカスタム関数をセットしておきます
+         @type {{}}
+         */
+        let setting = {};
+        setting.rule = {
+            // formatオブジェクトに、条件付き書式設定のカスタム数式をセットします
+            // キーはデータの型を指定します
+            format: {
+                "INTEGER": '=OR(isBlank($' + cellNotation + '2),ISNUMBER($' + cellNotation + '2)=FALSE,ISTEXT($' + cellNotation + '2)=TRUE,ISDATE($' + cellNotation + '2)=TRUE)',
+                "STRING": '=OR(isBlank($' + cellNotation + '2),ISNUMBER($' + cellNotation + '2)=TRUE,ISTEXT($' + cellNotation + '2)=FALSE,ISDATE($' + cellNotation + '2)=TRUE)',
+                "DATE": '=OR(isBlank($' + cellNotation + '2),ISNUMBER($' + cellNotation + '2)=TRUE,ISTEXT($' + cellNotation + '2)=FALSE,ISDATE($' + cellNotation + '2)=FALSE,REGEXMATCH($' + cellNotation + '2,"/")=TRUE)'
+            }
+        }
+
+        // REQUIRED配列をループして、必要な比較ができるようにします
+        for (let field = 1; field < requiredArray.length; field++) {
+            // 参照先スプレッドシートの項目列と、descriptionが一致している場合、書式設定とデータ入力をセットします
+            if (headerData === requiredArray[field][1]) {
+                _setFormatRule(lastRow, field, targetSheet, setting, headerData);
+
+                for (let cell = 2; cell < lastRow + 1; cell++) {
+                    setting.rule = {
+                        // inpuオブジェクトに、データ入力規則のカスタム数式をセットします
+                        // キーはデータの型を指定します
+                        input: {
+                            "INTEGER": '=AND(ISNUMBER($' + cellNotation + cell + ')=TRUE,ISTEXT($' + cellNotation + cell + ')=FALSE,ISDATE($' + cellNotation + cell + ')=FALSE)',
+                            "STRING": '=AND(ISNUMBER($' + cellNotation + cell + ')=FALSE,ISTEXT($' + cellNotation + cell + ')=TRUE,ISDATE($' + cellNotation + cell + ')=FALSE)',
+                            "DATE": '=AND(ISNUMBER($' + cellNotation + cell + ')=FALSE,ISTEXT($' + cellNotation + cell + ')=TRUE,ISDATE($' + cellNotation + cell + ')=TRUE,REGEXMATCH($' + cellNotation + cell + ',"-")=TRUE)'
+                        }
+                    }
+                    _setInputRule(cell, field, targetSheet, setting, headerData);
+                }
+            }
+        }
+    }
+}
+
+/**
+ * 参照先スプレッドシートに対して、条件付き書式設定をセットします
+ * @param lastRow
+ * @param key
+ * @param targetSheet
+ * @param setting
+ * @param header
+ * @private
+ */
+function _setFormatRule(lastRow, key, targetSheet, setting, header) {
+    let formatRule = SpreadsheetApp.newConditionalFormatRule()
+        // 警告色セット
+        .setBackground('#FF0000')
+        // セットするカスタム数式
+        .whenFormulaSatisfied(setting.rule.format[requiredArray[key][0]])
+        // 適用範囲
+        .setRanges([targetSheet.getRange(cellNotation + '2:' + cellNotation + lastRow + '')])
+        .build();
+    var rules = targetSheet.getConditionalFormatRules();
+    rules.push(formatRule);
+    targetSheet.setConditionalFormatRules(rules);
+}
+
+/**
+ * 参照先スプレッドシートに対して、データの入力規則をセットします
+ * @param cell
+ * @param key
+ * @param targetSheet
+ * @param setting
+ * @param header
+ * @private
+ */
+function _setInputRule(cell, key, targetSheet, setting, header) {
+    targetCell = cell;
+    let inputRule = SpreadsheetApp.newDataValidation()
+        // セットするカスタム数式
+        .requireFormulaSatisfied(setting.rule.input[requiredArray[key][0]])
+        // 無効なデータの場合
+        .setAllowInvalid(false)
+        .build();
+    targetSheet.getRange(cellNotation + cell).setDataValidation(inputRule);
+}
